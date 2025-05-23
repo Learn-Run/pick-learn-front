1f069609b0ea2f4fc71ec4fba06ec2a886a91417
Sutaenghhh <neocop7@gmail.com>
2025-05-23 11:30:18 +0900

    test: design system test

--- apps/web/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    '../../packages/ui/src/**/*.{js,ts,jsx,tsx}',
-  ],
-  presets: [require('../../packages/ui/tailwind.config.js')],
-}; 
